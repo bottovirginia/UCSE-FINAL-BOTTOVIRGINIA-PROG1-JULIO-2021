585ff9d4675ebf2db55e6c6e9aebabd28176b1d2
Maria Virginia Botto <bottovirginia@gmail.com>
2021-07-14 18:30:13 -0300

    COMMIT
    COMMIT

--- Envios.cs
+++ Envios.cs
@@ -6,15 +6,35 @@ using System.Threading.Tasks;
 
 namespace UCSE_FINAL_BOTTOVIRGINIA_PROG1_JULIO_2021
 {
-    public class Envios
+    public  class Envios
     {
-        public int numEnvio { get; set};
-        public int dniDestinatario { get, set};
-        //, un DNI de destinatario,
-        //un dni de repartidor,
-        //un estado de envío,
-        //una fecha estimada de entrega,
-        //una fecha de entrega y
-        //un historial de eventos asociados al envío(tipo evento y fecha del evento).
+        public int numEnvio { get; set; }
+        public int dniDestinatario { get; set; }
+        public int dniRepartidor { get; set; }
+        public eventosEnvios estadoEnvio { get; set; }
+        public DateTime fechaEstEntrega { get; set; }
+        public DateTime fechaEntrega { get; set; }
+        public eventosEnvios historialEventos { get; set; }
+        public int codigoSeguimiento{get;set;}
+        public string descPaquete { get; set; }
+    
+
+        public enum eventosEnvios
+        {
+           PENDIENTE_ENVIO = 0, LLEGADA_AL_CENTRO_DE_DISTRIBUCION = 1, EN_VIAJE = 2, EN_MANOS_DEL_REPARTIDOR = 3, ENTREGADO=4
+        }
+
+        public  Envios (int codSeguieminto, int dniCliente,  DateTime fechaEstEnt, string desc, eventosEnvios estadoInicial)
+        {
+            codSeguieminto = numEnvio;
+            dniCliente = dniDestinatario;
+            fechaEstEnt = fechaEstEntrega;
+            desc = descPaquete;
+            estadoInicial = eventosEnvios.PENDIENTE_ENVIO;
+
+        }
+
+
     }
+
 }
