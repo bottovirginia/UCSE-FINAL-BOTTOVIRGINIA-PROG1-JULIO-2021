dbf51c1617f4877ce2f5582d5fefd41685005786
Maria Virginia Botto <bottovirginia@gmail.com>
2021-07-14 17:22:48 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UCSE_FINAL_BOTTOVIRGINIA_PROG1_JULIO_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
