318b8c26a50705b89c9182f1d6759af804fa0cfa
Maria Virginia Botto <bottovirginia@gmail.com>
2021-07-14 18:15:51 -0300

    COMMIT
    COMMIT

---
+++ Destinatario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCSE_FINAL_BOTTOVIRGINIA_PROG1_JULIO_2021
+{
+    class Destinatario
+    {
+    }
+}
---
+++ Envios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCSE_FINAL_BOTTOVIRGINIA_PROG1_JULIO_2021
+{
+    public class Envios
+    {
+        public int numEnvio { get; set};
+        public int dniDestinatario { get, set};
+        //, un DNI de destinatario,
+        //un dni de repartidor,
+        //un estado de envío,
+        //una fecha estimada de entrega,
+        //una fecha de entrega y
+        //un historial de eventos asociados al envío(tipo evento y fecha del evento).
+    }
+}
